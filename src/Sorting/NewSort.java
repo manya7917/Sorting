239eeef2230c752d28c5a8253a07bc2833344aed
manya7917 <manya7917@gmail.com>
2022-09-19 14:53:27 +0530

    New Sort

---
+++ src/Sorting/NewSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+
+class NewSort {
+
+ public static void main(String[] args)
+ {
+
+     // Custom input array
+     int arr[] = { 45,89,23,1 };
+
+     // Outer loop
+     for (int i = 0; i < arr.length; i++) {
+
+         // Inner nested loop pointing 1 index ahead
+         for (int j = i + 1; j < arr.length; j++) {
+
+             // Checking elements
+             int temp = 0;
+             if (arr[j] < arr[i]) {
+
+                 // Swapping
+                 temp = arr[i];
+                 arr[i] = arr[j];
+                 arr[j] = temp;
+             }
+         }
+
+         
+         System.out.print(arr[i] + " ");
+     }
+ }
+}
